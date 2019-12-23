66970c06039a2b3dbc21dd9506cb5babf572c914
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-12-23 13:50:25 +0530

    update configs

--- config_settings.php
+++ config_settings.php
@@ -1,8 +1,8 @@
 <?php
 // Enter your MySQL settings below
 $mysql_server = "localhost";
-$mysql_user = "root";
-$mysql_password = "aco1234";
+$mysql_user = "pi";
+$mysql_password = "2wsxzaq1";
 $mysql_database = "acoserver_games";
-$SITETITLE = "Aerodia";
+$SITETITLE = "Phaos RPG";
 ?>
